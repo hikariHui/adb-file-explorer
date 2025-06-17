d90183063fe507e56d6adfc669e86f747bd94a04
huangminhui <huangminhui@37.com>
2025-06-17 11:04:26 +0800

    feat: init

---
+++ stylelint.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    "stylelint-config-property-sort-order-smacss",
+    "stylelint-config-standard",
+    "stylelint-config-recommended-vue",
+  ],
+};
